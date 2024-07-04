dfb531990a6d069f015d17bcdc3d9b8150b9768d
George <george@arrowsmith.codes>
2024-07-04 09:35:23 +0100

    add/remove messages with pubsub

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -186,6 +186,8 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_params(params, _session, socket) do
+    if socket.assigns[:room], do: Chat.unsubscribe_from_room(socket.assigns.room)
+
     room =
       case Map.fetch(params, "id") do
         {:ok, id} ->
@@ -196,6 +198,8 @@ defmodule SlaxWeb.ChatRoomLive do
       end
 
     messages = Chat.list_messages_in_room(room)
+
+    Chat.subscribe_to_room(room)
 
     {:noreply,
      assign(socket,
@@ -212,9 +216,9 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_event("delete-message", %{"id" => id}, socket) do
-    {:ok, message} = Chat.delete_message_by_id(id, socket.assigns.current_user)
+    Chat.delete_message_by_id(id, socket.assigns.current_user)
 
-    {:noreply, stream_delete(socket, :messages, message)}
+    {:noreply, socket}
   end
 
   def handle_event("submit-message", %{"message" => message_params}, socket) do
@@ -222,10 +226,8 @@ defmodule SlaxWeb.ChatRoomLive do
 
     socket =
       case Chat.create_message(room, message_params, current_user) do
-        {:ok, message} ->
-          socket
-          |> stream_insert(:messages, message)
-          |> assign_message_form(Chat.change_message(%Message{}))
+        {:ok, _message} ->
+          assign_message_form(socket, Chat.change_message(%Message{}))
 
         {:error, changeset} ->
           assign_message_form(socket, changeset)
@@ -242,5 +244,13 @@ defmodule SlaxWeb.ChatRoomLive do
     changeset = Chat.change_message(%Message{}, message_params)
 
     {:noreply, assign_message_form(socket, changeset)}
+  end
+
+  def handle_info({:new_message, message}, socket) do
+    {:noreply, stream_insert(socket, :messages, message)}
+  end
+
+  def handle_info({:message_deleted, message}, socket) do
+    {:noreply, stream_delete(socket, :messages, message)}
   end
 end
