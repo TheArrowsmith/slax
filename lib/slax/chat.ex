74a36cd98001bdf8a4aea60971e4b0373389b136
George <george@arrowsmith.codes>
2025-03-20 10:31:44 +0000

    add context functions to create and update rooms

--- lib/slax/chat.ex
+++ lib/slax/chat.ex
@@ -1,7 +1,7 @@
 defmodule Slax.Chat do
   alias Slax.Chat.Room
   alias Slax.Repo
-  
+
   import Ecto.Query
 
   def list_rooms do
@@ -10,5 +10,17 @@ defmodule Slax.Chat do
 
   def get_room!(id) do
     Repo.get!(Room, id)
+  end
+
+  def create_room(attrs) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_room(%Room{} = room, attrs) do
+    room
+    |> Room.changeset(attrs)
+    |> Repo.update()
   end
 end
