4521d55c3ba25d5886bb2b241c896c5f13152c94
George <george@arrowsmith.codes>
2025-06-12 13:54:11 +0100

    add observer to extra applications

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Slax.MixProject do
   def application do
     [
       mod: {Slax.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :observer, :wx]
     ]
   end
 
